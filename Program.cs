8ed105eb17900efccb2320f35357089d4e1ebe53
Nameuser9 <ivamand2002@gmail.com>
2022-10-11 14:39:19 +0700

    Создание массива из текста и его печать
    (пока что нет никаких условий)

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿void Fillarray(string[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = Convert.ToString(Console.ReadLine());                   //new Random().Next(-1000, 1001);
+        index++;
+    }
+}
+void PrintArray(string[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+       Console.WriteLine(col[position]);
+       position++; 
+    }
+}
