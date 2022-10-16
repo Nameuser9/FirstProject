321ac0bc08e5c98e2d76267ba3eb9f50384b3b51
Nameuser9 <ivamand2002@gmail.com>
2022-10-17 00:48:53 +0700

    Финальный код

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     int index = 0;
     while (index < length)
     {
-        collection[index] = Convert.ToString(Console.ReadLine());                   //new Random().Next(-1000, 1001);
+        collection[index] = Convert.ToString(Console.ReadLine());                   //Ввод слов в массив
         index++;
     }
 }
@@ -14,7 +14,17 @@ void PrintArray(string[] col)
     int position = 0;
     while (position < count)
     {
+        if (col[position].Length < 4)   //Выводится слова которые короче 4 симбволов
+        {
        Console.WriteLine(col[position]);
+        }
        position++; 
     }
 }
+Console.WriteLine("введите сколько слов будет в текстовом массиве");
+int m =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводите слова");
+string[] array = new string[m];
+Fillarray(array);
+Console.WriteLine("Слова из 3 и менее симбволов");
+PrintArray(array);
